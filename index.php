43196cd93a0e8ae31fcc73c2f1c3014f85cda36e
viktor-osvoll <viktor.osvoll@medieinstitutet.se>
2020-09-16 15:39:06 +0200

    all functions fixed

--- index.php
+++ index.php
@@ -2,10 +2,43 @@
 $context = Timber::get_context();
 $context['posts'] = new Timber\PostQuery();
 $context['foo'] = 'bar';
+$context['fields'] = get_fields(10);
+
+// recipe connection
+$args_recipes = array(
+// Get post type project
+'post_type' => 'recipes',
+// Get all posts
+'posts_per_page' => -1,
+// Order by post date
+'orderby' => array(
+    'date' => 'ASC'
+));
+
+$context['recipes'] = Timber::get_posts( $args_recipes );
+/* $repeater = $post->get_field( 'recipe_ingredients' ); */
+
+
+// Product connection
+
+$args_products = array(
+    // Get post type project
+    'post_type' => 'products',
+    // Get all posts
+    'posts_per_page' => -1,
+    // Order by post date
+    'orderby' => array(
+        'date' => 'ASC'
+    ));
+    
+    $context['products'] = Timber::get_posts( $args_products );
+    /* $repeater = $post->get_field( 'recipe_ingredients' ); */
+
+
 $templates = array( 'index.twig' );
 
 if ( is_home() ) {
     array_unshift( $templates, 'home.twig' );
 }
 
-Timber::render( $templates, $context );
+Timber::render( $templates, /* 'layouts/recipe.twig', */ $context );
--- single.php
+++ single.php
@@ -3,6 +3,39 @@
 $context = Timber::get_context();
 $post = Timber::query_post();
 $context['post'] = $post;
+$context['product'] = get_field($post->ID);
+$context['recipe'] = get_field($post->ID);
+
+
+// recipe connection
+$args_product = array(
+// Get post type project
+'post_type' => 'products',
+// Get all posts
+'posts_per_page' => -1,
+// Order by post date
+'orderby' => array(
+    'date' => 'ASC'
+));
+
+$context['products'] = Timber::get_posts( $args_product );
+/* $repeater = $post->get_field( 'recipe_ingredients' ); */
+
+
+// recipe connection
+$args_recipes = array(
+    // Get post type project
+    'post_type' => 'recipes',
+    // Get all posts
+    'posts_per_page' => -1,
+    // Order by post date
+    'orderby' => array(
+        'date' => 'ASC'
+    ));
+    
+    $context['recipes'] = Timber::get_posts( $args_recipes );
+    /* $repeater = $post->get_field( 'recipe_ingredients' ); */
+
 
 if ( post_password_required( $post->ID ) ) {
     Timber::render( 'single/single-password.twig', $context );
